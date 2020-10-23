1496ceb84aac780f7612918f1fe2d46931f57d8c
ThanhNam2905 <thanhnam290596@gmail.com>
2020-10-23 18:39:59 +0800

    Updates

---
+++ precache-manifest.5afb7994876990cfb6196caa16cbe403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3c56b7526d27207e3ef18bdb749d38",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "a3f7141027547316048a",
+    "url": "/movie-app/static/css/main.381178f2.chunk.css"
+  },
+  {
+    "revision": "88c2253776e8f71c401d",
+    "url": "/movie-app/static/js/2.5d65da4b.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/movie-app/static/js/2.5d65da4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f7141027547316048a",
+    "url": "/movie-app/static/js/main.21bd2299.chunk.js"
+  },
+  {
+    "revision": "424abb17f479c7475584",
+    "url": "/movie-app/static/js/runtime-main.1c26a08d.js"
+  }
+]);
