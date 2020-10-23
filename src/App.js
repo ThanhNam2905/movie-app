d3873cbd9b2a02ac99f2b9457e2e589bfe2adbf9
ThanhNam2905 <thanhnam290596@gmail.com>
2020-10-23 17:53:17 +0800

    Commit Vote_Average Style CSS

--- src/App.js
+++ src/App.js
@@ -11,31 +11,57 @@ function App() {
   const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
   const [movies, setMovies] = useState([]);
+  const [search_keyword, setSearch_keyword] = useState([]);
 
   useEffect(async () => {
-    fetch(FEATURED_API)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setMovies(data.results);
-      });
-
+      getMovies(FEATURED_API);
   }, [])
 
-  return (
-    <>
-    <header>
-      <input className="search" type="search"  placeholder="Search movies..."/>
-    </header>
+  const getMovies = (API) => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+        setMovies(data.results);
+      });
+  }
 
-    <div className="Movie-App">
-      <div className="movie-container">
-        {movies.length > 0 && movies.map(movie => (
-          <Movie key={movie.id} {...movie}/>
-        ))}
+  // Bat su kien OnSubmit khi user tim kiem phim
+  const handleOnSubmit = (event) => {
+    event.preventDefault(); // giup User khi submit form se ko mat di keyword vua search
+
+    if(search_keyword) {
+      getMovies(SEARCH_API + search_keyword);
+      // setSearch_keyword("");
+    }
+  }
+
+  // Bat su kien OnChange khi value search_keyword thay doi
+  const handleOnChange = (event) => {
+    // console.log(event.target.value);
+    setSearch_keyword(event.target.value);
+  }
+
+  return (
+    <div>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input className="search" 
+                 type="search"  
+                 placeholder="Search movies..."
+                 value={search_keyword}
+                 onChange={handleOnChange}/>
+        </form>
+      </header>
+
+      <div className="Movie-App">
+        <div className="movie-container">
+          {movies.length > 0 && movies.map(movie => (
+            <Movie key={movie.id} {...movie}/>
+          ))}
+        </div>
       </div>
     </div>
-    </>
   );
 }
 
