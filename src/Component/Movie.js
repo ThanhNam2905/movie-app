0c0ef4ee598dcf3a8eb23df5cdf10a18db27225d
ThanhNam2905 <thanhnam290596@gmail.com>
2020-10-23 18:23:54 +0800

    Complete Project Movie App

--- src/Component/Movie.js
+++ src/Component/Movie.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 const IMAGE_API = "https://image.tmdb.org/t/p/w1280";
 const Movie = ({title, poster_path, overview, vote_average}) => {
+
+    const setVote_Average = (vote) => {
+        if(vote >= 8) return "green"
+        else if(vote >= 6) return "orange"
+        else return "red"
+    }
+
     return (
     <div className="movie">
-        <img src={IMAGE_API + poster_path} alt={title}/>
+        <img src={(poster_path) ? (IMAGE_API + poster_path) : 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQdabjye5wnu90Ph8mfzwhRcfvjSCukSC6GNg&usqp=CAU'} alt={title}/>
         <div className="movie-info">
             <h2>{title}</h2>
-            <span>{vote_average}</span>
+            <span className={`vote-Rage ${setVote_Average(vote_average)}`}>{vote_average}</span>
         </div>
         <div className="movie-overview">
             <h3>Overview:</h3>
